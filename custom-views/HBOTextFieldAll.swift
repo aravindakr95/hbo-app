83088742cc853e81496aba089d9b576ffdc629e8
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-01-14 10:47:43 +0530

    Update fonts and text field place holder logic

--- custom-views/HBOTextFieldAll.swift
+++ custom-views/HBOTextFieldAll.swift
@@ -26,3 +26,14 @@ class HBOTextFieldAll: UITextField {
     }
     
 }
+
+extension UITextField{
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: newValue!])
+        }
+    }
+}
--- custom-views/HBOTextFieldTop.swift
+++ custom-views/HBOTextFieldTop.swift
@@ -28,14 +28,3 @@ class HBOTextFieldTop: UITextField {
 
 }
 
-extension UITextField{
-    @IBInspectable var placeHolderColor: UIColor? {
-        get {
-            return self.placeHolderColor
-        }
-        set {
-            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: newValue!])
-        }
-    }
-}
-
